11b45980685f219752154423fa0c71a3c13ce5c5
Elmeliani-y <youssefelmeliani55@gmail.com>
2025-12-16 17:49:58 +0100

    fix: Add route to serve storage files directly

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
+// Serve storage files directly to avoid RoutingController catching them
+Route::get('storage/{path}', function ($path) {
+    $file = storage_path('app/public/' . $path);
+    if (file_exists($file)) {
+        return response()->file($file);
+    }
+    abort(404);
+})->where('path', '.*');
 <?php
 
 use App\Http\Controllers\BranchController;
